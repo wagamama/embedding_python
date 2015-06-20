1e8460f0ea8cd66c54a9874e98a470aab3e4a643
Bruce Tsai <wagamama.tsai@gmail.com>
2015-06-20 17:05:14 +0800

    Add example of callback function

--- tutorial-6.c
+++ tutorial-6.c
@@ -1,7 +1,6 @@
 #include <Python.h>
 #include <stdio.h>
 
-static char* MODULE_NAME = "cmodule";
 static char* FUNCTION_NAME = "callback";
 static PyObject* callback(PyObject* self, PyObject* args)
 {
@@ -12,13 +11,13 @@ static PyObject* callback(PyObject* self, PyObject* args)
     Py_RETURN_NONE;
 }
 
-static char* PY_MODULE_NAME = "tutorial-5";
+static char* PY_MODULE_NAME = "tutorial-6";
 static char* PY_FUNCTION_NAME = "multiply";
 
 int main(int argc, char* argv[])
 {
     int a, b;
-    PyObject *pModule = NULL, *pFunc = NULL, *pArgs = NULL;
+    PyObject *pCallbackFunc = NULL, *pModule = NULL, *pFunc = NULL, *pArgs = NULL;
 
     Py_Initialize();
     PyObject* sysPath = PySys_GetObject((char*) "path");
@@ -29,11 +28,9 @@ int main(int argc, char* argv[])
 
     do
     {
-        PyMethodDef CFunctions[] = {
-            {FUNCTION_NAME, callback, METH_VARARGS, ""},
-            {NULL, NULL, 0, NULL}
-        };
-        Py_InitModule(MODULE_NAME, CFunctions);
+        PyMethodDef CFunc = {FUNCTION_NAME, callback, METH_VARARGS, ""};
+        pCallbackFunc = PyCFunction_New(&CFunc, NULL);
+        if (pCallbackFunc == NULL) break;
 
         pModule = PyImport_ImportModule(PY_MODULE_NAME);
         if (pModule == NULL) break;
@@ -41,13 +38,14 @@ int main(int argc, char* argv[])
         pFunc = PyObject_GetAttrString(pModule, PY_FUNCTION_NAME);
         if (pFunc == NULL) break;
 
-        pArgs = Py_BuildValue("ii", a, b);
+        pArgs = Py_BuildValue("iiO", a, b, pCallbackFunc);
         if (pArgs == NULL) break;
 
         PyObject_Call(pFunc, pArgs, NULL);
         break;
     } while (1);
 
+    Py_XDECREF(pCallbackFunc);
     Py_XDECREF(pArgs);
     Py_XDECREF(pFunc);
     Py_XDECREF(pModule);
