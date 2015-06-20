e9c5c5a568dd6d3dcf33fe948550e05fe0327650
Bruce Tsai <wagamama.tsai@gmail.com>
2015-06-20 12:33:18 +0800

    Add thread in c example

--- tutorial-3.c
+++ tutorial-3.c
@@ -1,62 +1,58 @@
 #include <Python.h>
 #include <stdio.h>
+#include <pthread.h>
 
 static char* PY_MODULE_NAME = "tutorial-34";
-static char* PY_THREAD_MANAGER_CLASS = "ThreadManager";
-static char* PY_START_THREAD_FUNCTION = "start_thread";
-static char* PY_STOP_THREAD_FUNCTION = "stop_thread";
+static char* PY_WORKING_FUNCTION = "working";
 
-int main(int argc, char* argv[])
+int stop_event = 0;
+
+void* run_python_function(void* arg)
 {
-    PyObject *pModule = NULL, *pClass = NULL, *pInst = NULL;
-    PyGILState_STATE state;
+    PyObject *pModule = NULL, *pFunc = NULL;
 
-    PyEval_InitThreads();
     Py_Initialize();
     PyObject* sysPath = PySys_GetObject((char*) "path");
     PyList_Append(sysPath, PyString_FromString("."));
 
-    PyThreadState* save = PyEval_SaveThread();
-
     do
     {
-        state = PyGILState_Ensure();
+        pModule = PyImport_ImportModule(PY_MODULE_NAME);
+        if (pModule == NULL) break;
+
+        pFunc = PyObject_GetAttrString(pModule, PY_WORKING_FUNCTION);
+        if (pFunc == NULL) break;
+
+        while (!stop_event)
         {
-            pModule = PyImport_ImportModule(PY_MODULE_NAME);
-            if (pModule == NULL) break;
-
-            pClass = PyObject_GetAttrString(pModule, PY_THREAD_MANAGER_CLASS);
-            if (pClass == NULL) break;
-
-            pInst = PyObject_CallObject(pClass, NULL);
-            if (pInst == NULL) break;
-
-            PyObject_CallMethod(pInst, PY_START_THREAD_FUNCTION, NULL);
-        }
-        PyGILState_Release(state);
-
-        for (int i = 0; i < 5; i++)
-        {
-            printf("main thread is running\n");
-            sleep(1);
+            PyObject_CallFunction(pFunc, "s", "worker");
         }
 
-        state = PyGILState_Ensure();
-        {
-            PyObject_CallMethod(pInst, PY_STOP_THREAD_FUNCTION, NULL);
-        }
-        PyGILState_Release(state);
-
-        printf("finish\n");
         break;
     } while (1);
 
-    PyEval_RestoreThread(save);
-
-    Py_XDECREF(pInst);
-    Py_XDECREF(pClass);
+    Py_XDECREF(pFunc);
     Py_XDECREF(pModule);
 
     Py_Finalize();
+
+    return NULL;
+}
+
+int main(int argc, char* argv[])
+{
+    pthread_t tid;
+    pthread_create(&tid, NULL, &run_python_function, NULL);
+
+    for (int i = 0; i < 5; i++)
+    {
+        printf("main thread is running\n");
+        sleep(1);
+    }
+
+    stop_event = 1;
+
+    printf("finish\n");
+
     return 0;
 }
